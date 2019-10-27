758a23f0cb7bb5a7981d60dd05ab90db52d556af
2018ht66557 <kolachana.sriramgopal@gmail.com>
2019-10-27 17:40:10 +0530

    p1, p2 python programs

---
+++ p1.py
@@ -0,0 +1,11 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from scipy.spatial.distance import cosine
+s1 = 'BITS WILP course on data structures'
+s2 = 'data mining related course at BITS WILP'
+
+cv = CountVectorizer()
+cv.fit([s1,s2])
+cvs1 = cv.transform([s1])  ##converts s1 into a vector
+cvs2 = cv.transform([s2]) ##converts s2 into a vector
+
+print 1-cosine(cvs1.todense(),cvs2.todense())
---
+++ p2.py
@@ -0,0 +1,21 @@
+
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+
+d = datasets.load_breast_cancer()
+B = d.data
+y = d.target
+
+X_train, X_test, y_train, y_test = train_test_split(B, y, test_size=0.2, random_state=42)
+
+clf =  DecisionTreeClassifier(random_state=42)
+clf.fit(X_train, y_train)
+pred = clf.predict(X_test)
+print metrics.f1_score(y_test,pred)
+
+scores = cross_val_score(clf, B, y, cv=5, scoring='f1')
+print scores.mean()
+print scores
